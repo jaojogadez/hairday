792cf7cb4be49769e04637dfa8e8e9c1dff68857
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-03-29 19:25:23 -0300

    add mais funcionalidades e interatividade

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,9 +1,12 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours.js";
+import { hoursClick } from "./hours-click.js";
 
 const $hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  // Limpar list de horários
+  $hours.innerHTML = ""
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora
     const [scheduleHour] = hour.split(":");
@@ -23,7 +26,24 @@ export function hoursLoad({ date }) {
     li.classList.add("hour");
     li.classList.add(available ? "hour-available" : "hour-unavailable");
     li.textContent = hour;
+
+    if (hour === "9:00") {
+      addHourHead("Manhã")
+    } else if (hour === "13:00"){
+      addHourHead("Tarde")
+    } else if (hour === "18:00"){
+      addHourHead("Noite")
+    }
+
     $hours.append(li)
-    
   });
+
+  hoursClick();
+}
+
+function addHourHead(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title
+  $hours.append(header);
 }
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 const form = document.querySelector("form");
 const $selectDate = document.getElementById("date");
+const $clientNameInput = document.getElementById("client");
 
 // Data atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -11,5 +12,38 @@ $selectDate.min = inputToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
-  alert("Enviado");
+  try {
+    // Recuperando o nome do cliente
+    const name = $clientNameInput.value.trim();
+
+    // Caso entre na condição, o return para tudo
+    if (!name) {
+      return alert("Informe o nome do cliente!");
+    }
+
+    // Recupera o horário selecionado
+    const $hourSelected = document.querySelector(".hour-selected");
+    console.log($hourSelected);
+
+    if (!$hourSelected) {
+      return alert("Selecione a hora.");
+    }
+
+    // Recuperar somente a hora
+    const [hour] = $hourSelected.textContent.split(":");
+
+    // Insere a hora na data
+    const when = dayjs($selectDate.value).add(hour, "hour");
+
+    // Gera um ID
+    const id = new Date().getTime();
+    console.log({
+      id,
+      name,
+      when
+    })
+
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+  }
 };
